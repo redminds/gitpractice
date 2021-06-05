8e277a836d499c5ec140f1e5036dbdaddac97e86
Ubuntu <leelaparameswar@leelaparameswar.mctq5cjlyocupcbgm2n01zvp2c.gx.internal.cloudapp.net>
2021-06-05 19:01:41 +0000

    get all countriues

---
+++ getcountries.java
@@ -0,0 +1,59 @@
+ckage com.webmitta.model;
+
+
+
+import java.util.Locale;
+
+
+
+public class ListCountry {
+
+
+
+    public static void main(String[] args) {
+
+
+
+    ListCountry obj = new ListCountry();
+
+    obj.run();
+
+
+
+    }
+
+
+
+    public void run() {
+
+
+
+    String[] locales = Locale.getISOCountries();
+
+
+
+    for (String countryCode : locales) {
+
+
+
+        Locale obj = new Locale("", countryCode);
+
+
+
+        System.out.println("Country Code = " + obj.getCountry() 
+
+            + ", Country Name = " + obj.getDisplayCountry());
+
+
+
+    }
+
+
+
+    System.out.println("Done");
+
+    }
+
+    
+
+}
