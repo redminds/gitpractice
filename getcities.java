d0424285f855249c057833c96e2e394c774f36a0
Ubuntu <leelaparameswar@leelaparameswar.mctq5cjlyocupcbgm2n01zvp2c.gx.internal.cloudapp.net>
2021-06-05 18:10:15 +0000

    This is  java class to get the city names

---
+++ getcities.java
@@ -0,0 +1,21 @@
+c class Country {
+
+    public java.util.List<String> getItems() {
+
+        java.util.List<String> stateList = new java.util.ArrayList<String>();
+
+        list.add("State1");
+
+        list.add("State2");
+
+        list.add("State3");
+
+        ------
+
+        ------ // add rest of the states
+
+        return stateList;
+
+    }
+
+}
